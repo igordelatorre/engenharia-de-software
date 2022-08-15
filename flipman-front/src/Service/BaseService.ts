dfe88a53eacee82285591c7459456c9ba81aaa8f
igordelatorre <delatorreigor0@gmail.com>
2022-08-14 21:14:03 -0300

    front: update baseUrl

--- flipman-front/src/Service/BaseService.ts
+++ flipman-front/src/Service/BaseService.ts
@@ -1,47 +1,49 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from "axios";
+
+const baseURL = process.env.NODE_ENV === "development" || !process.env.NODE_ENV
+  ? "https://localhost:7021"
+  : `${process.env.REACT_APP_API_BASE_URL}/api`;
 
 const api = axios.create({
-	baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`
-})
-
-
+  baseURL
+});
 
 class BaseService {
-	static post<Payload, Response>(
-		modelName: string,
-		data: Payload,
-		route = ''
-	): Promise<AxiosResponse<Response>> {
-		return api.post(`${modelName}${route}`, data)
-	}
+  static post<Payload, Response>(
+    modelName: string,
+    data: Payload,
+    route = ""
+  ): Promise<AxiosResponse<Response>> {
+    return api.post(`${modelName}${route}`, data);
+  }
 
-	static get<Response, Query>(
-		modelName: string,
-		route = '',
-		query?: Query,
-		responseType: 'json' | 'blob' = 'json'
-	): Promise<AxiosResponse<Response>> {
-		return api.get(`${modelName}${route}`, {
-			params: query,
-			responseType: responseType
-		})
-	}
+  static get<Response, Query>(
+    modelName: string,
+    route = "",
+    query?: Query,
+    responseType: "json" | "blob" = "json"
+  ): Promise<AxiosResponse<Response>> {
+    return api.get(`${modelName}${route}`, {
+      params: query,
+      responseType: responseType,
+    });
+  }
 
-	static put<Payload, Response>(
-		modelName: string,
-		data: Payload,
-		route = ''
-	): Promise<AxiosResponse<Response>> {
-		return api.put(`${modelName}${route}`, data)
-	}
+  static put<Payload, Response>(
+    modelName: string,
+    data: Payload,
+    route = ""
+  ): Promise<AxiosResponse<Response>> {
+    return api.put(`${modelName}${route}`, data);
+  }
 
-	static remove<Payload, Response>(
-		modelName: string,
-		data: Payload,
-		route = ''
-	): Promise<AxiosResponse<Response>> {
-		return api.delete(`${modelName}${route}`, { data })
-	}
+  static remove<Payload, Response>(
+    modelName: string,
+    data: Payload,
+    route = ""
+  ): Promise<AxiosResponse<Response>> {
+    return api.delete(`${modelName}${route}`, { data });
+  }
 }
 
-export default BaseService
+export default BaseService;

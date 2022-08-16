e725d613b320aa969cf225453378209e7166fb18
igordelatorre <delatorreigor0@gmail.com>
2022-08-15 21:36:05 -0300

    Front: prettier

--- flipman-front/src/Web/Pages/Players/PlayerSidebar.tsx
+++ flipman-front/src/Web/Pages/Players/PlayerSidebar.tsx
@@ -1,80 +1,79 @@
-import { Col, Form, Row } from 'antd'
-import { FormikHelpers, useFormik } from 'formik'
-import React, { useEffect, useState } from 'react'
-import handlers from '../../Components/handlers'
-import validate from './validate'
-import { SidebarButtonContainer } from '../../Styles/style'
-import Player, {PlayerFactory, IncompletePlayerFactory} from '../../../Domain/Player'
-import {SidebarPlayer} from '../../Components/Sidebar/style'
-import Input from '../../Components/Input/Input'
-import ConfirmButton from '../../Components/Button/ConfirmButton'
-import CancelButton from '../../Components/Button/CancelButton'
+import { Col, Form, Row } from "antd";
+import { FormikHelpers, useFormik } from "formik";
+import handlers from "../../Components/handlers";
+import validate from "./validate";
+import { SidebarButtonContainer } from "../../Styles/style";
+import Player, {
+  PlayerFactory,
+  IncompletePlayerFactory,
+} from "../../../Domain/Player";
+import { SidebarPlayer } from "../../Components/Sidebar/style";
+import Input from "../../Components/Input/Input";
+import ConfirmButton from "../../Components/Button/ConfirmButton";
+import CancelButton from "../../Components/Button/CancelButton";
 
 type Props = {
-	isOpen: boolean
-	onClose(): void
-    addPlayer: (player: Player) => void
+  isOpen: boolean;
+  onClose(): void;
+  addPlayer: (player: Player) => void;
+};
+
+function PlayerSidebar({ isOpen, onClose, addPlayer }: Props) {
+  const onSubmit = async (
+    values: Partial<Player>,
+    formik: FormikHelpers<Partial<Player>>
+  ) => {
+    const newPlayer = PlayerFactory(values);
+    formik.resetForm();
+    addPlayer(newPlayer);
+    onClose();
+  };
+
+  const formik = useFormik<Partial<Player>>({
+    initialValues: IncompletePlayerFactory({}),
+    onSubmit,
+    validate: (values: Partial<Player>) => validate(values),
+    enableReinitialize: true,
+  });
+
+  return (
+    <SidebarPlayer
+      header={"Novo Jogador"}
+      isOpen={isOpen}
+      onClose={onClose}
+      width={"47.5rem"}
+    >
+      <Form onSubmitCapture={(e) => e.preventDefault()} layout="vertical">
+        <Row gutter={16}>
+          <Col xs={12} lg={12}>
+            <Form.Item label={"Nome"}>
+              <Input {...handlers.string(formik, "name")} />
+            </Form.Item>
+          </Col>
+          <Col xs={12} lg={12}>
+            <Form.Item label={"Email"}>
+              <Input {...handlers.string(formik, "email")} />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col xs={12} lg={12}>
+            <Form.Item label={"Telefone"}>
+              <Input {...handlers.string(formik, "cellphone")} />
+            </Form.Item>
+          </Col>
+          <Col xs={12} lg={12}>
+            <Form.Item label={"Cartão"}>
+              <Input {...handlers.number(formik, "card")} />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      <SidebarButtonContainer>
+        <CancelButton onClick={onClose} children={"Cancel"} />
+        <ConfirmButton onClick={formik.submitForm} children={"Salvar"} />
+      </SidebarButtonContainer>
+    </SidebarPlayer>
+  );
 }
-
-function PlayerSidebar({ isOpen, onClose, addPlayer}: Props) {
-
-	const onSubmit = async (
-		values: Partial<Player>,
-		formik: FormikHelpers<Partial<Player>>
-	) => {
-		const newPlayer = PlayerFactory(values)
-		formik.resetForm()
-		addPlayer(newPlayer)
-		onClose()
-	}
-
-	const formik = useFormik<Partial<Player>>({
-		initialValues: IncompletePlayerFactory({}),
-		onSubmit,
-		validate: (values: Partial<Player>) => validate(values),
-		enableReinitialize: true
-	})
-
-	return (
-		<SidebarPlayer
-			header={'Novo Jogador'}
-			isOpen={isOpen}
-			onClose={onClose}
-			width={'47.5rem'}
-		>
-			<Form onSubmitCapture={e => e.preventDefault()} layout="vertical">
-				<Row gutter={16}>
-					<Col xs={12} lg={12}>
-						<Form.Item label={'Nome'}>
-							<Input {...handlers.string(formik, 'name')} />
-						</Form.Item>
-					</Col>
-					<Col xs={12} lg={12}>
-						<Form.Item label={'Email'}>
-							<Input {...handlers.string(formik, 'email')} />
-						</Form.Item>
-					</Col>
-                </Row>
-				<Row gutter={16}>
-					<Col xs={12} lg={12}>
-						<Form.Item label={'Telefone'}>
-							<Input {...handlers.number(formik, 'cellphone')} />
-						</Form.Item>
-					</Col>
-					<Col xs={12} lg={12}>
-						<Form.Item label={'Cartão'}>
-							<Input {...handlers.number(formik, 'card')} />
-						</Form.Item>
-					</Col>
-                </Row>
-
-				
-			</Form>
-			<SidebarButtonContainer>
-				<CancelButton onClick={onClose} children={"Cancel"} />
-				<ConfirmButton onClick={formik.submitForm} children={"Salvar"} />
-			</SidebarButtonContainer>
-		</SidebarPlayer>
-	)
-}
-export default PlayerSidebar
+export default PlayerSidebar;
--- flipman-front/src/Web/Pages/Players/PlayersPage.tsx
+++ flipman-front/src/Web/Pages/Players/PlayersPage.tsx
@@ -1,75 +1,69 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-	ContentContainer,
-	ContentMenu,
-	AlignedPageButtonContainer,
-	PageContainer,
-	PageTitle,
-	PageTitleContainer
-} from '../../Styles/style'
-import Player from '../../../Domain/Player'
-import PlayersTable from './Table/PlayersTable'
-import ConfirmButton from '../../Components/Button/ConfirmButton'
-import PlayerSidebar from './PlayerSidebar'
-import FixedCard from '../../Components/FixedCard/FixedCard'
-import PlayerService from '../../../Service/PlayerService'
-import { ResponsePlayer } from '../../../Service/PlayerService'
+  ContentContainer,
+  ContentMenu,
+  AlignedPageButtonContainer,
+  PageContainer,
+  PageTitle,
+  PageTitleContainer,
+} from "../../Styles/style";
+import Player from "../../../Domain/Player";
+import PlayersTable from "./Table/PlayersTable";
+import ConfirmButton from "../../Components/Button/ConfirmButton";
+import PlayerSidebar from "./PlayerSidebar";
+import FixedCard from "../../Components/FixedCard/FixedCard";
+import PlayerService from "../../../Service/PlayerService";
+import { ResponsePlayer } from "../../../Service/PlayerService";
 
-
-const id = 'holidays-page'
+const id = "holidays-page";
 
 function PlayersPage() {
+  const [sidebar, setSidebar] = useState<boolean>(false);
 
-	const [sidebar, setSidebar] = useState<boolean>(false)
+  const openAddSidebar = () => setSidebar(true);
 
-	const openAddSidebar = () => setSidebar(true)
+  const handleCloseSidebar = () => setSidebar(false);
 
-	const handleCloseSidebar = () => setSidebar(false)
+  const [players, setPlayers] = useState<ResponsePlayer[]>([]);
 
-	const [players, setPlayers] = useState<ResponsePlayer[]>([])
-	
-	const getAllPlayers = async () => 
-	{
-		const response = await PlayerService.getAll()
-		setPlayers(response)
+  const getAllPlayers = async () => {
+    const response = await PlayerService.getAll();
+    setPlayers(response);
+  };
 
-	}
+  const addPlayer = async (newPlayer: Player) => {
+    await PlayerService.add(newPlayer);
+    getAllPlayers();
+  };
 
+  useEffect(() => {
+    getAllPlayers();
+  }, []);
 
-	const addPlayer = async (newPlayer: Player) => 
-	{
-		await PlayerService.add(newPlayer);
-		getAllPlayers()
-	}
-
-	getAllPlayers();
-
-	return (
-			<PageContainer id={id}>
-				<ContentContainer id={id}>
-					<PageTitleContainer>
-						<PageTitle>{'Jogadores'}</PageTitle>
-					</PageTitleContainer>
-					<FixedCard>
-						<ContentMenu>
-							<AlignedPageButtonContainer>
-								<ConfirmButton onClick={openAddSidebar}>
-									{'Novo Jogador'}
-								</ConfirmButton>
-							</AlignedPageButtonContainer>
-							<PlayersTable
-								players={players}
-							/>
-							<PlayerSidebar
-								isOpen={sidebar}
-								onClose={handleCloseSidebar}
-                                addPlayer={addPlayer}
-							/>
-						</ContentMenu>
-					</FixedCard>
-				</ContentContainer>
-			</PageContainer>
-	)
+  return (
+    <PageContainer id={id}>
+      <ContentContainer id={id}>
+        <PageTitleContainer>
+          <PageTitle>{"Jogadores"}</PageTitle>
+        </PageTitleContainer>
+        <FixedCard>
+          <ContentMenu>
+            <AlignedPageButtonContainer>
+              <ConfirmButton onClick={openAddSidebar}>
+                {"Novo Jogador"}
+              </ConfirmButton>
+            </AlignedPageButtonContainer>
+            <PlayersTable players={players} />
+            <PlayerSidebar
+              isOpen={sidebar}
+              onClose={handleCloseSidebar}
+              addPlayer={addPlayer}
+            />
+          </ContentMenu>
+        </FixedCard>
+      </ContentContainer>
+    </PageContainer>
+  );
 }
 
-export default PlayersPage
+export default PlayersPage;

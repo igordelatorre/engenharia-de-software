2da94a45865ad5eed2243e23b18959c9707538d0
igordelatorre <delatorreigor0@gmail.com>
2022-08-17 22:40:42 -0300

    Revert "Api: add env to access db"
    This reverts commit 59c6309978499b03fc6361439735105c75ae5537.

--- flipman-api/Program.cs
+++ flipman-api/Program.cs
@@ -14,27 +14,11 @@ internal class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        String? connectionString;
+        builder.Services.AddDbContext<FlipmanDbContext>(
+            o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        );
 
-        if (builder.Environment.IsProduction())
-        {
-            connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-
-            if (connectionString == null)
-            {
-                throw new Exception("DB_CONNECTION_STRING_NOT_FOUND");
-            }
-
-        }
-        else
-        {
-            connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        }
-
-        builder.Services.AddDbContext<FlipmanDbContext>(o => o.UseNpgsql(connectionString));
-
-        builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-        {
+        builder.Services.AddCors(p => p.AddPolicy("corsapp", builder => {
             builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
         }));
 

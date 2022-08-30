0c17d9f40e859730a699d99c760cb8d9c62f499c
Igor De Latorre <50248007+igordelatorre@users.noreply.github.com>
2022-08-30 11:44:31 -0300

    Update create-flipman-db.sql

--- flipman-db/create-flipman-db.sql
+++ flipman-db/create-flipman-db.sql
@@ -5,6 +5,7 @@ CREATE TABLE players(
     card int NOT NULL,
     tokens int NOT NULL DEFAULT 0,
     tickets int NOT NULL DEFAULT 0,
+    isActive boolean NOT NULL DEFAULT true,   
     cellphone char(11)
 );
 
@@ -12,7 +13,8 @@ CREATE TABLE machines(
     id SERIAL PRIMARY KEY, 
     name varchar(255) NOT NULL,
     playCost int NOT NULL DEFAULT 0,
-    pointsPerTicket int NOT NULL DEFAULT 0
+    pointsPerTicket int NOT NULL DEFAULT 0,
+    isActive boolean NOT NULL DEFAULT true   
 );
 
 CREATE TABLE matches(
@@ -21,6 +23,7 @@ CREATE TABLE matches(
     machineId int NOT NULL, 
     points int NOT NULL,
     playTime int NOT NULL,
+    datetime timestamp NOT NULL, 
     CONSTRAINT fk_player FOREIGN KEY (playerId) REFERENCES players(id),
     CONSTRAINT fk_machine FOREIGN KEY (machineId) REFERENCES machines(id)
 );
@@ -79,17 +82,19 @@ CREATE TABLE machineEvents(
     managerId int NOT NULL,
     machineId int NOT NULL,
     eventType varchar(255) NOT NULL,
+    datetime timestamp NOT NULL,
     CONSTRAINT fk_manager FOREIGN KEY (managerId) REFERENCES employees(id),
     CONSTRAINT fk_machine FOREIGN KEY (machineId) REFERENCES machines(id),
     CONSTRAINT fk_management_event FOREIGN KEY (eventType) REFERENCES managementTypes(type)
 );
 
-CREATE TABLE machineEvents(
+CREATE TABLE prizeEvents(
     id SERIAL PRIMARY KEY,
     managerId int NOT NULL,
-    machineId int NOT NULL,
+    prizeId int NOT NULL,
+    datetime timestamp NOT NULL,
     eventType varchar(255) NOT NULL,
     CONSTRAINT fk_manager FOREIGN KEY (managerId) REFERENCES employees(id),
-    CONSTRAINT fk_machine FOREIGN KEY (machineId) REFERENCES machines(id),
+    CONSTRAINT fk_prize FOREIGN KEY (prizeId) REFERENCES prizes(id),
     CONSTRAINT fk_management_event FOREIGN KEY (eventType) REFERENCES managementTypes(type)
 );

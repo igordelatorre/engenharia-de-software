e52103730ba32a45e9d4b1fb999dbe94de05d97d
Henrique-Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2022-09-03 17:25:00 -0300

    Criando o model do Prize

---
+++ flipman-api/Models/Prize.cs
@@ -0,0 +1,9 @@
+namespace Flipman.Api.Models;
+
+public class Prize
+{
+  public int id {get; set;}
+  public string? name {get; set;}
+  public int amount {get; set;}
+  public int price {get; set;}
+}

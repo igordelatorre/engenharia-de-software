dd6559f47ef6317327f294e42e25bdba276a882d
joaopfp <joaopedro_ferreirapereira@hotmail.com>
2022-09-11 21:02:15 -0300

    Tela de Login Adicionada

--- flipman-front/src/index.tsx
+++ flipman-front/src/index.tsx
@@ -4,12 +4,15 @@ import 'antd/dist/antd.css'
 import './Web/Styles/general.css'
 import App from "./App"
 import {BrowserRouter} from "react-router-dom"
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

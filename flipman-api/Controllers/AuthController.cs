6e9a93dc03666545215a3bf4b635097a6f855dac
igordelatorre <delatorreigor0@gmail.com>
2022-09-12 18:29:27 -0300

    api: create employees controller

--- flipman-api/Controllers/AuthController.cs
+++ flipman-api/Controllers/AuthController.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using Flipman.Api.Authorization;
 using Flipman.Api.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -19,61 +18,9 @@ public class AuthController : ControllerBase
         DbContext = dbContext;
     }
 
-    [HttpGet]
-    [Authorize]
-    [Route("me")]
-    public async Task<IActionResult> Me()
-    {
-        var foo = User.FindFirstValue(AppClaims.AdminId);
-
-        return Ok(foo);
-    }
-
-    [HttpGet]
-    [Route("employees")]
-    // [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> GetEmployees()
-    {
-        var employees = await DbContext.Employees.ToArrayAsync();
-
-        return Ok(employees);
-    }
-
     [HttpPost]
-    [Route("register")]
-    public async Task<IActionResult> RegisterEmployee([FromBody] RegisterEmployeeRequest request)
-    {
-        if (string.IsNullOrEmpty(request.username) || string.IsNullOrEmpty(request.password))
-            return BadRequest("INVALID_INPUT");
-
-        CreatePasswordHash(request.password, out byte[] passwordHash, out byte[] passwordSalt);
-
-        var newEmployee = new Employee
-        {
-            username = request.username,
-            passwordhash = passwordHash,
-            passwordsalt = passwordSalt,
-            name = request.name,
-            isadmin = request.isAdmin ?? false
-        };
-
-        await DbContext.Employees.AddAsync(newEmployee);
-        await DbContext.SaveChangesAsync();
-
-        return Ok(newEmployee);
-    }
-
-    public class RegisterEmployeeRequest
-    {
-        public string? username { get; set; }
-        public string? password { get; set; }
-        public string? name { get; set; }
-        public bool? isAdmin { get; set; }
-    }
-
-    [HttpPost]
-    [Route("login")]
-    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    [Route("login/employee")]
+    public async Task<IActionResult> EmployeeLogin([FromBody] EmployeeLoginRequest request)
     {
         if (string.IsNullOrEmpty(request.username) || string.IsNullOrEmpty(request.password))
             return BadRequest("INVALID_INPUT");
@@ -95,20 +42,34 @@ public class AuthController : ControllerBase
         return Ok(token);
     }
 
-    public class LoginRequest
+    public class EmployeeLoginRequest
     {
         public string? username { get; set; }
         public string? password { get; set; }
     }
 
-
-    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    [HttpPost]
+    [Route("login/player")]
+    public async Task<IActionResult> PlayerLogin([FromBody] PlayerLoginRequest request)
     {
-        using (var hmac = new HMACSHA512())
+        if (request.card == null)
+            return BadRequest("INVALID_INPUT");
+
+        var player = await DbContext.Players.Where(p => p.card == request.card).FirstOrDefaultAsync();
+
+        if (player == null)
         {
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return BadRequest();
         }
+
+        var token = CreateToken(player);
+
+        return Ok();
+    }
+
+    public class PlayerLoginRequest
+    {
+        public long? card;
     }
 
     private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
@@ -125,7 +86,29 @@ public class AuthController : ControllerBase
         List<Claim> claims = new List<Claim>
         {
             new Claim(ClaimTypes.Name, employee.username),
-            new Claim(AppClaims.AdminId, employee.id.ToString())
+            new Claim(AppClaims.ManagerId, employee.id.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("foobarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+
+    private string CreateToken(Player player)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(AppClaims.PlayerId, player.id.ToString())
         };
 
         var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("foobarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"));

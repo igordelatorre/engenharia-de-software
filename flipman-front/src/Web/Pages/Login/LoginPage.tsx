0436bcb2c1455a634fae3e0642a4855ef3337e52
joaopfp <68359715+joaopfp@users.noreply.github.com>
2022-09-13 15:59:40 -0300

    Tela de Login Adicionada do funcionário adicionada (#27)
    * Added Machine Middleware Service
    
    * Tela de Login Adicionada

--- flipman-front/src/Web/Pages/Login/LoginPage.tsx
+++ flipman-front/src/Web/Pages/Login/LoginPage.tsx
@@ -1,7 +1,53 @@
-function LoginPage() {
-
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  
+export default function LoginPage() {
     return (
-        <h1>Página Login.</h1>
-    )
-}
-export default LoginPage
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="name">
+                <FormLabel>Nome</FormLabel>
+                <Input type="name" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Senha</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Entrar
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }

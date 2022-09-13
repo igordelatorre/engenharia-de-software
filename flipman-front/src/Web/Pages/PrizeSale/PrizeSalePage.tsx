6ac3bab333ac113eb9cdea3cd1e4ee1a7e630c68
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-09-12 21:59:23 -0300

    Input de cartão do jogador #29

--- flipman-front/src/Web/Pages/PrizeSale/PrizeSalePage.tsx
+++ flipman-front/src/Web/Pages/PrizeSale/PrizeSalePage.tsx
@@ -1,7 +1,40 @@
+import { useState, FormEvent } from "react"
+import Prize from "../../../Domain/Prize"
+import { CenteredContentContainer, ContentContainer, HorizontalCenteredContainer, PageContainer } from "../../Styles/style"
+import { Input, Button } from '@chakra-ui/react'
+
+enum PageState {
+    CARD,
+    TABLE
+}
+
 function PrizeSalePage() {
+    const [selectedPrize, setSelectedPrize] = useState<Prize>()
+    const [playerCard, setPlayerCard] = useState<string | null>("")
+    const [pageState, setPageState] = useState<PageState>(PageState.CARD)
+
+
+    function handleSubmitPlayerCard() {
+        setPageState(PageState.TABLE)
+    }
 
     return (
-        <h1>Página de venda de prêmios, restrita ao funcionário. Ler o cartão do jogador, e mostrar somente a lista de prêmios que ele pode comprar, e permitir a compra.</h1>
+        <PageContainer>
+            <CenteredContentContainer>
+                {pageState === PageState.CARD && (
+                    <HorizontalCenteredContainer>
+                        <form onSubmit={handleSubmitPlayerCard}>
+                            <Input width="50vw" placeholder={"Cartão do jogador"} onChange={(e : FormEvent<HTMLInputElement>) => {setPlayerCard(e.currentTarget.value)}}/>
+                            <Button type="submit">Buscar</Button>
+                        </form>
+                    </HorizontalCenteredContainer>
+                )}
+                {pageState === PageState.TABLE && (
+                    <h1>Prizes que o {playerCard} pode comprar</h1>
+                )}
+            </CenteredContentContainer>
+                
+        </PageContainer>
     )
 }
 export default PrizeSalePage

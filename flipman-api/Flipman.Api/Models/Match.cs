089251ce3082eeaa22b722b6a171dab49e97f4e4
igordelatorre <delatorreigor0@gmail.com>
2022-09-28 03:45:07 -0300

    api: update match model

--- flipman-api/Flipman.Api/Models/Match.cs
+++ flipman-api/Flipman.Api/Models/Match.cs
@@ -5,9 +5,7 @@ public class Match
     public int Id { get; set; }
     public int PlayerCard { get; set; }
     public int MachineId { get; set; }
-    public int Points { get; set; }
     public int PlayTime { get; set; }
     public DateTime Datetime { get; set; }
-    public int Tokens { get; set; }
     public int Tickets { get; set; }
 }

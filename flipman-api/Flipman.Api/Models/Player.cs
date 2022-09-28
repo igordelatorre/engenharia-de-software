a109b0fd90c008e12a971f68da0e42fb1e200844
igordelatorre <delatorreigor0@gmail.com>
2022-09-28 00:01:09 -0300

    api: update PostPlayer

--- flipman-api/Flipman.Api/Models/Player.cs
+++ flipman-api/Flipman.Api/Models/Player.cs
@@ -10,4 +10,16 @@ public class Player
     public int Tickets { get; set; }
     public bool IsActive { get; set; }
     public string? Cellphone { get; set; }
+
+    public Player(int card, string name, string username, string email, string? cellphone)
+    {
+        Card = card;
+        Username = username;
+        Name = name;
+        Email = email;
+        Cellphone = cellphone;
+        Tokens = 0;
+        Tickets = 0;
+        IsActive = true;
+    }
 }

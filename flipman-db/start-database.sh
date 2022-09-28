545f5c4c7a8b72a138f3d2890f49579831dffce0
igordelatorre <delatorreigor0@gmail.com>
2022-09-27 23:29:29 -0300

    db: update start-database script

--- flipman-db/start-database.sh
+++ flipman-db/start-database.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker build -t flipman/db --file flipman-db.Dockerfile .
+# sudo systemctl restart docker.socket docker.service
+# sudo ss -lptn 'sport = :5432'
+# sudo kill -9 pid
 
+docker build -t flipman/db --file flipman-db.Dockerfile . && \
+docker rm -f postgres && \
 docker run --name=postgres --restart=always -p 5432:5432 -d flipman/db

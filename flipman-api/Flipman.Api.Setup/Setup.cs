255d0b673a0a23cca03005d865cbfc9f56c35aa1
igordelatorre <delatorreigor0@gmail.com>
2022-09-28 22:51:52 -0300

    api.setup: update

--- flipman-api/Flipman.Api.Setup/Setup.cs
+++ flipman-api/Flipman.Api.Setup/Setup.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using Flipman.Api.Models;
 using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flipman.Api.Setup;
 
@@ -11,23 +12,76 @@ public class Setup
     {
         var dbContext = new AppDbContext();
 
-        CreatePasswordHash("123456", out byte[] passwordHash, out byte[] passwordSalt);
+        ClearDatabase(dbContext);
+
+        CreatePasswordHash("123456", out byte[] adminPasswordHash, out byte[] adminPasswordSalt);
 
         var admin = new Employee
         {
             Name = "adm",
             Username = "admin",
-            PasswordHash = passwordHash,
-            PasswordSalt = passwordSalt,
+            PasswordHash = adminPasswordHash,
+            PasswordSalt = adminPasswordSalt,
             IsAdmin = true
         };
         await dbContext.Employees.AddAsync(admin);
 
-        var player = new Player(44, "fulano", "fulano", "fulano@email.com", "55999887710");
+        CreatePasswordHash("123456", out byte[] employeePasswordHash, out byte[] employeePasswordSalt);
+
+        var employee = new Employee
+        {
+            Name = "atendente",
+            Username = "atendente",
+            PasswordHash = employeePasswordHash,
+            PasswordSalt = employeePasswordSalt,
+            IsAdmin = true
+        };
+        await dbContext.Employees.AddAsync(employee);
+
+        var playerCard = 44;
+
+        var player = new Player(playerCard, "fulano", "fulano", "fulano@email.com", "55999887710");
         await dbContext.Players.AddAsync(player);
+
+        var player2 = new Player(11, "josé teste", "jose_teste", "teste@email.com", "55999887710");
+        await dbContext.Players.AddAsync(player2);
 
         var machine = new Machine("PacMan", 5);
         await dbContext.Machines.AddAsync(machine);
+
+        await dbContext.SaveChangesAsync();
+
+        var machineId = await dbContext.Machines.Select(m => m.Id).FirstOrDefaultAsync();
+
+        var match = new Match
+        {
+            MachineId = machineId,
+            PlayerCard = playerCard,
+            Datetime = System.DateTime.UtcNow,
+            PlayTime = 80,
+            Tickets = 22
+        };
+        await dbContext.Matches.AddAsync(match);
+
+        var match2 = new Match
+        {
+            MachineId = machineId,
+            PlayerCard = playerCard,
+            Datetime = System.DateTime.UtcNow.AddDays(-35),
+            PlayTime = 80,
+            Tickets = 22
+        };
+        await dbContext.Matches.AddAsync(match2);
+
+        var match3 = new Match
+        {
+            MachineId = machineId,
+            PlayerCard = playerCard,
+            Datetime = System.DateTime.UtcNow.AddDays(-8),
+            PlayTime = 33,
+            Tickets = 11
+        };
+        await dbContext.Matches.AddAsync(match3);
 
         await dbContext.SaveChangesAsync();
     }
@@ -39,5 +93,15 @@ public class Setup
             passwordSalt = hmac.Key;
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         }
+    }
+
+    private void ClearDatabase(AppDbContext dbContext)
+    {
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM players");
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM machines");
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM matches");
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM prizes");
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM prizes_transactions");
+        dbContext.Database.ExecuteSqlRaw("DELETE FROM employees");
     }
 }

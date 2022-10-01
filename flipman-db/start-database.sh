3dad9debff573904e2d7f1545c6898cbda0de5b1
igordelatorre <delatorreigor0@gmail.com>
2022-10-01 18:50:29 -0300

    ata-2022-10-01

--- flipman-db/start-database.sh
+++ flipman-db/start-database.sh
@@ -7,3 +7,6 @@
 docker build -t flipman/db --file flipman-db.Dockerfile . && \
 docker rm -f postgres && \
 docker run --name=postgres --restart=always -p 5432:5432 -d flipman/db
+
+# sudo docker run --name=postgres -e POSTGRES_USER=flipman -e POSTGRES_PASSWORD=123456 --restart=always -p 5432:5432 -d postgres
+# sudo docker run --name=api --restart=always --net=host -d igordelatorre/flipman
